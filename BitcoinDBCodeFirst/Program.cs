f333609d6b1b39c48c0c50b69f60762440489365
Trainer <trainer@stepit.loc>
2018-05-08 18:51:43 +0300

    Initial commit

---
+++ BitcoinDBCodeFirst/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace BitcoinDBCodeFirst
+{
+    class Program
+    {
+        static void ParseJson(string filename)
+        {
+            //TODO build structures based on the json
+        }
+        
+        static void StoreData()
+        {
+            //TODO Add to Database Transactions and Deposits
+        }
+
+        static void RetrieveDeposits()
+        {
+            //TODO Output at console deposits
+        }
+
+        static void Main(string[] args)
+        {
+            ParseJson("transaction.json");
+
+            StoreData();
+
+            RetrieveDeposits();
+        }
+    }
+}
